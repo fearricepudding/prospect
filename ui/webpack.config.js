37089a5c04ab6a74e3b81976b3169ac20c904566
Pyr0 <jordanrandles@googlemail.com>
2021-05-26 10:03:21 +0100

    Merged node packages

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./ui/src/index.js",
 	mode: "development",
 	resolve: {
 		alias: {
